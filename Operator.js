cfa39eb07c35bbf2a607f7c9c7467d26aefffa55
Vivekkumar-pandey <vpandey442003@gmail.com>
2026-03-22 13:01:55 +0530

    Fourth-Class

---
+++ Operator.js
@@ -0,0 +1,219 @@
+// Operators *********************
+// a + b 
+//  
+
+// Arithematic********
+// + , - , * , / , % , **
+
+// console.log(10 + 20);
+// console.log(20 - 10);
+// console.log(20 * 10);
+// console.log(25 / 10);
+// console.log(Math.floor(25 / 10));
+
+// console.log(25 % 10);
+
+// console.log(5 ** 2);
+// console.log(2 ** 5);
+
+
+// Relational ***********************
+// > , < , >= , <= , == , != , !== , ===
+
+// console.log(10 > 20);
+// console.log(10 < 20);
+// console.log(10 == 20);
+
+// console.log(10 >= 20);
+// console.log(10 <= 20);
+// console.log(10 != 20);
+
+// let a = '2';
+// let b = 2;
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+// console.log(a == b);
+
+
+
+// console.log('2' == 2);
+
+// console.log('2' === 2);
+
+// console.log('2' != 2);
+// console.log('2' !== 2);
+
+// Assignment *************
+// = , += , -= , *= , /= , %= **=
+
+// let a ;
+// a = 20;
+// console.log(a);
+
+
+// let a = 10;
+
+// a = a + 30;
+
+// a += 30;
+// a -= 5;
+// a *= 5;
+//  a /= 5;
+//   a %= 5;
+
+// let a = 5;
+// // a **= 2;
+// console.log(a);
+
+// logicaL **********
+// AND && ,OR || ,NOT !
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// console.log(true  || true);
+// console.log(true  || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// console.log(!true);
+// console.log(!false);
+
+
+//  Unary ***************
+
+// Pre -> incriment -> ++i , decriment ->  --i
+// Post -> incriment -> i++ , decriment ->  i++
+
+
+// let a = 10;
+// a++;
+// console.log(a);
+
+// let a = 10;
+// console.log(a++);
+// console.log(a);
+
+// let a = 10;
+// // a = a++;
+// a = ++a;
+// console.log(a);
+
+
+// let a = 10;
+
+// console.log(a++);
+// console.log(a);
+
+// console.log(++a);
+// console.log(a);
+
+
+
+// let a = 22;
+// a++;
+
+// let b = a++;
+
+// let c = ++b;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+// let y = 17;
+
+// y = y++ ;
+// y= y++ ;
+// y = y++ ;
+
+// y = y--;
+// y = y++;
+// y = y--;
+// y = y-- ;
+
+// console.log(y);
+
+
+// let a = 2;
+
+// let sum = a++ + a++ + ++a;
+// console.log(sum);
+
+
+// Ternary Operator *********
+
+// (condition) ? true : false ;
+
+// let num = 9;
+// let result = (num % 2 === 0 ) ? "Even" : "Odd";
+// console.log(result);
+
+
+// let age = 15;
+// let result = (age >= 18) ? "You Can Vote" : "You Can't Vote";
+// console.log(result);
+
+
+// let age = 35;
+
+// let result = (age >= 18 && age <= 50 ) ? "You Can Drive" : "You Cann't Drive" ;
+
+// console.log(result);
+
+
+
+// ??
+
+// console.log(12 || 0);
+
+// console.log(null ?? 7);
+// console.log(undefined ?? 0);
+
+// console.log(7 ?? null);
+// console.log(0 ?? undefined);
+
+
+
+// typeOf
+// instanceOf
+
+// BitWise ********
+// & , | , ~  , ^ , >> , << 
+
+// console.log(5 & 1);   // 5 -> 101  1 - 1   => 001 -> 1 
+
+// console.log(7 & 1);
+
+// console.log(5 | 1); // // 5 -> 101  1 - 1   => 101 -> 
+
+// console.log(6 | 2); 
+
+// console.log(5 ^ 1);  
+
+// console.log(5 >> 1);
+
+// console.log(7 >> 1);
+
+// console.log(5 << 1);
+
+// console.log(7 << 2);
+
+// console.log(~5);
+
+console.log(~9);
+
+
+
+
+
+
+
+
