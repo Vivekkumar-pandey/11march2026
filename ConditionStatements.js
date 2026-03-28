7e220b6f650664582430f9172f0bab60b9259ddd
Vivekkumar-pandey <vpandey442003@gmail.com>
2026-03-28 17:45:11 +0530

    .

--- ConditionStatements.js
+++ ConditionStatements.js
@@ -128,41 +128,41 @@ Fail !!
 
 
 
-let day = 90;
+// let day = 90;
 
-switch (day) {
-    case 1: {
-        console.log("Sunday");
-        break;
-    }
-    case 2: {
-        console.log("Monday");
-        break;
-    }
-    case 3: {
-        console.log("Tuesday");
-        break;
-    }
-    case 4: {
-        console.log("Wednesday");
-        break;
-    }
-    case 5: {
-        console.log("Thrusday");
-        break;
-    }
-    case 6: {
-        console.log("Friday");
-        break;
-    }
-    case 7: {
-        console.log("Saturday");
-        break;
-    }
-    default: {
-        console.log("Invalid Day Value !!");
-    }
-}
+// switch (day) {
+//     case 1: {
+//         console.log("Sunday");
+//         break;
+//     }
+//     case 2: {
+//         console.log("Monday");
+//         break;
+//     }
+//     case 3: {
+//         console.log("Tuesday");
+//         break;
+//     }
+//     case 4: {
+//         console.log("Wednesday");
+//         break;
+//     }
+//     case 5: {
+//         console.log("Thrusday");
+//         break;
+//     }
+//     case 6: {
+//         console.log("Friday");
+//         break;
+//     }
+//     case 7: {
+//         console.log("Saturday");
+//         break;
+//     }
+//     default: {
+//         console.log("Invalid Day Value !!");
+//     }
+// }
 
 
 
