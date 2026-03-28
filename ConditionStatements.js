92429f7c63a4be7a31e015220dc886d01ebc0cd4
Vivekkumar-pandey <vpandey442003@gmail.com>
2026-03-28 12:55:51 +0530

    Fifth Class

---
+++ ConditionStatements.js
@@ -0,0 +1,179 @@
+// if *******************
+
+// let n = 11;
+// if(n % 2 === 0){
+//     console.log(n,"Is Even Number");
+
+// }
+
+
+// let n = 11;
+// if(n % 2 !== 1){
+//     console.log(n,"Is Odd Number");
+
+// }
+
+// let n = 11;
+// if(n % 2 === 1){
+//     console.log(n,"Is Odd Number");
+
+// }
+
+// if else ************
+
+// let n = 13;
+
+// if(n % 2 === 0){
+//     console.log(n,"Is Even Number !!");
+
+// }
+// else{
+//     console.log(n,"Is Odd Number !!");
+// }
+
+
+// let n = 10;
+
+// if(n % 2 === 0)
+//     console.log(n,"Is Even Number !!");
+
+// else
+//     console.log(n,"Is Odd Number !!");
+
+
+
+// else if ladder ****************
+
+/*
+if(condition){
+
+}
+else if(condition){
+
+}
+
+else if(condition){
+
+}
+else{
+
+}
+*/
+
+/*
+
+90 > A
+80 > B
+70 > C
+60 > D
+Fail !!
+
+*/
+
+// let marks = -30;
+
+// if(marks > 100 || marks < 0){
+//     console.log("Invalid Marks !!");
+// }
+// else if(marks >= 90){
+//     console.log("Grade : A");
+
+// }
+// else if(marks >= 80) {
+//      console.log("Grade : B");
+// }
+// else if(marks >= 70) {
+//      console.log("Grade : C");
+// }
+// else if(marks >= 60) {
+//      console.log("Grade : D");
+// }
+// else{
+//     console.log("Fail !!");
+// }
+
+
+// Nested Conditional Statements
+
+// let num = 12;
+
+// if (num > 0) {
+//     console.log(num, "Is Positive Number !!");
+
+//     if (num % 2 === 0) {
+//         console.log(num, "Is Positive Even Number !!");
+//     }
+//     else {
+//         console.log(num, "Is Positive Odd Number !!");
+//     }
+// }
+// else {
+//     console.log(num, "Is Negative Number !!");
+
+//     if (num % 2 === 0) {
+//         console.log(num, "Is Negative Even Number !!");
+//     }
+//     else {
+//         console.log(num, "Is Negative Odd Number !!");
+//     }
+// }
+
+// Leap Year 
+// Even And Odd Without %
+// Swap a = 10, b = 20; 
+
+
+// Switch 
+// switch , case , break, default
+
+
+
+let day = 90;
+
+switch (day) {
+    case 1: {
+        console.log("Sunday");
+        break;
+    }
+    case 2: {
+        console.log("Monday");
+        break;
+    }
+    case 3: {
+        console.log("Tuesday");
+        break;
+    }
+    case 4: {
+        console.log("Wednesday");
+        break;
+    }
+    case 5: {
+        console.log("Thrusday");
+        break;
+    }
+    case 6: {
+        console.log("Friday");
+        break;
+    }
+    case 7: {
+        console.log("Saturday");
+        break;
+    }
+    default: {
+        console.log("Invalid Day Value !!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
