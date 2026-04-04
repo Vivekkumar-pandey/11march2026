946ba27b1595041c1d2654872ed086dd4d6e0c02
Vivekkumar-pandey <vpandey442003@gmail.com>
2026-04-04 13:01:00 +0530

    Seven Class Function

---
+++ Function.js
@@ -0,0 +1,83 @@
+// Function ***************
+
+// Normal *****
+
+// greet();
+
+// function greet(){
+//     console.log("Good After-Noon");
+    
+// }
+
+// greet();
+
+// parametrized ******
+
+// function getSum(a, b) {
+//     return a + b;
+// }
+
+// let result = getSum(12, 30);
+// console.log(result);
+
+// console.log(getSum(12, 34));
+
+
+
+// function getSum(a, b) {
+//     console.log("Get Sum Method *****");
+    
+//     return a + b;
+
+//     //   console.log("Get Sum Method *****");   // UnreachableCode/ Dead Code *****
+
+// }
+
+// console.log(getSum(12, 34));
+
+
+
+
+// Function Expression **************
+
+// getSum(20,30); // ❌
+
+// let getSum = function (a,b) {
+//     console.log(a+b);
+    
+// }
+
+// console.log(getSum);
+
+// getSum(20,30)
+
+
+// IIFE ()() ****************
+
+// (function(){
+//     console.log("IIFE Function");
+    
+// })()
+
+
+// (function (a,b) {
+//     console.log(a+b);
+    
+// })(56 , 90)
+
+
+// Arrow Function ()=>{} ************
+
+// let result = (n)=>{
+//     return n*2;
+// }
+
+// let result = (n)=>  n*2;
+
+// let result = n =>  n*2;
+
+// let result = (n)=> { return n*2 }
+
+// console.log(result(5));
+
+
